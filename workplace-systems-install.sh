38c3fabb62bb27960411400574fd905425bfd554
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-05 18:17:19 +0100

    pointed to wsys workstation

--- workplace-systems-install.sh
+++ workplace-systems-install.sh
@@ -5,5 +5,5 @@ sudo php cleopatra/install-silent
 cd /var/www
 git clone https://github.com/phpengine/workplace-systems
 cd workplace-systems
-sudo cleopatra auto x --af="build/config/cleopatra/cleofy/autopilots/custom/Phlagrant/cleofy-cm-cap-workstation.php"
+sudo cleopatra auto x --af="build/config/cleopatra/cleofy/autopilots/custom/Phlagrant/cleofy-cm-wsys-workstation.php"
 phlagrant up now
