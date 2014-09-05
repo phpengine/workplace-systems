61b75f87add65b1147fe2dd22759dcd1810f89ad
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-05 17:49:30 +0100

    create job for wsys

--- build/config/jenkins/create_job.sh
+++ build/config/jenkins/create_job.sh
@@ -1,5 +1,5 @@
-sudo mkdir -p /var/lib/jenkins/jobs/capgemini_tests
-sudo cp /var/www/workplace-systems/build/config/jenkins/config.xml /var/lib/jenkins/jobs/capgemini_tests/config.xml
-sudo chmod -R 755 /var/lib/jenkins/jobs/capgemini_tests
-sudo chown -R jenkins /var/lib/jenkins/jobs/capgemini_tests
-sudo chgrp -R jenkins /var/lib/jenkins/jobs/capgemini_tests
+sudo mkdir -p /var/lib/jenkins/jobs/workplace-systems_tests
+sudo cp /var/www/workplace-systems/build/config/jenkins/config.xml /var/lib/jenkins/jobs/workplace-systems_tests/config.xml
+sudo chmod -R 755 /var/lib/jenkins/jobs/workplace-systems_tests
+sudo chown -R jenkins /var/lib/jenkins/jobs/workplace-systems_tests
+sudo chgrp -R jenkins /var/lib/jenkins/jobs/workplace-systems_tests
