31a354e7cb1a21f87195f812d9cb10ab7932a4b2
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-05 18:18:11 +0100

    jenkins vhost typo

--- build/config/dapperstrano/dapperfy/autopilots/custom/phlagrant-box-jenkins-vhost.php
+++ build/config/dapperstrano/dapperfy/autopilots/custom/phlagrant-box-jenkins-vhost.php
@@ -70,7 +70,6 @@ class AutoPilotConfigured extends AutoPilot {
     ProxyPreserveHost On
     ProxyPass / http://127.0.0.1:8080/
     ProxyPassReverse / http://127.0.0.1:8080/
-    ServerName hostname.example.com
 </VirtualHost>
 
 TEMPLATE;
