0b49829468e6558d8f5bc66865b8c7f556fcc4b2
David Amanshia <dave@pharoah-tools.org.uk>
2014-09-09 16:52:26 +0100

    phlagrant box matches updates from other projct

--- build/config/cleopatra/cleofy/autopilots/generic/Phlagrant/cleofy-cm-phlagrant-box.php
+++ build/config/cleopatra/cleofy/autopilots/generic/Phlagrant/cleofy-cm-phlagrant-box.php
@@ -44,7 +44,7 @@ class AutoPilotConfigured extends AutoPilot {
                 array ( "ApacheModules" => array( "ensure" => array(),),),
 
                 // Apache Modules
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our common Apache Modules are installed" ),),),
+                array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure our Reverse Proxy Apache Modules are installed" ),),),
                 array ( "ApacheReverseProxyModules" => array( "ensure" => array(),),),
 
                 // Standard Tools
@@ -59,7 +59,7 @@ class AutoPilotConfigured extends AutoPilot {
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Git SSH Key Safe version is are installed" ),),),
                 array ( "GitKeySafe" => array( "ensure" => array(),),),
 
-                // Firefox 17
+                // Firefox 24
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets ensure Firefox 24 is installed for selenium" ),),),
                 array ( "Firefox24" => array( "ensure" => array(),),),
 
@@ -106,13 +106,6 @@ class AutoPilotConfigured extends AutoPilot {
                     "command" => 'cp /var/www/workplace-systems/build/config/jenkins/global/* /var/lib/jenkins',
                 ),),),
 
-                // copy to jenkins home
-                array ( "Logging" => array( "log" => array( "log-message" => "Lets create our jenkins job"),),),
-                array ( "RunCommand" => array("install" => array(
-                    "guess" => true,
-                    "command" => 'cp /var/www/workplace-systems/build/config/jenkins/global/* /var/lib/jenkins',
-                ),),),
-
                 // Bash script to create jenkins job
                 // ususlly would use a platform independent method, but time
                 array ( "Logging" => array( "log" => array( "log-message" => "Lets create our jenkins job"),),),
@@ -153,8 +146,14 @@ class AutoPilotConfigured extends AutoPilot {
                     "mysql-host" => "127.0.0.1"
                 ) ) ),
 
+                // @todo surely, captain, there must be a better way...
                 array ( "Logging" => array( "log" => array(
-                    "log-message" => "Cleopatra Configuration Management of your Phlagrant VM complete"
+                    "log-message" => "Here's a bug... but not one of mine... due to the following firefox bug - ".
+                    "https://bugzilla.mozilla.org/show_bug.cgi?id=198863 when the phlagrant user tries to open ".
+                    "Firefox, the profile dir is created as root and the Firefox program (opened as phlagrant) cannot ".
+                    "open or write to it. Anyhow, we have to start firefox as that user, let it fail, delete the root ".
+                    "profile dir then on next opening it will work".
+                    " "
                 ),),),
 
 
@@ -162,16 +161,23 @@ class AutoPilotConfigured extends AutoPilot {
                 array ( "Logging" => array( "log" => array( "log-message" => "the mozilla dir ends up being owned by root somehow, so fix" ),),),
                 array ( "RunCommand" => array( "install" => array(
                     "guess" => true,
+                    "run-as-user" => "phlagrant",
                     "command" => "chown -R phlagrant /home/phlagrant/.mozilla",
                 ) ) ),
+
                 array ( "RunCommand" => array( "install" => array(
                     "guess" => true,
-                    "command" => "chgrp -R phlagrant /home/phlagrant/.mozilla",
+                    "command" => "rm -rf /home/phlagrant/.mozilla",
                 ) ) ),
+
                 array ( "RunCommand" => array( "install" => array(
                     "guess" => true,
                     "command" => "chmod -R 775 /home/phlagrant/.mozilla",
                 ) ) ),
+
+                array ( "Logging" => array( "log" => array(
+                    "log-message" => "Cleopatra Configuration Management of your Phlagrant VM complete"
+                ),),),
 
             );
 
